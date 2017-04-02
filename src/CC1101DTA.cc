938925f6438daf63bdf9eb2d926b78030bee5d7f
Yegor Gorshkov <gorshkov@gmail.com>
2017-04-02 10:32:49 -0700

    Stop logging packets/setup on each packet

--- src/CC1101DTA.cc
+++ src/CC1101DTA.cc
@@ -13,62 +13,64 @@ uint8_t CC1101DTA::chkSum() {
 }
 time_t CC1101DTA::decode() {
 	// returns 0 if data can't be decoded; UNIX timestamp on success.
-  const static char XDIGIT[] = "0123456789abcdef";
-  const static uint8_t XLAT[] {
-    0B10101010, 0B01101010, 0B10011010, 0B01011010, 
-    0B10100110, 0B01100110, 0B10010110, 0B01010110, 
-    0B10101001, 0B01101001, 0B10011001, 0B01011001, 
-    0B10100101, 0B01100101, 0B10010101, 0B01010101,
-  };
+	const static char XDIGIT[] = "0123456789abcdef";
+	const static uint8_t XLAT[] {
+		0B10101010, 0B01101010, 0B10011010, 0B01011010, 
+		0B10100110, 0B01100110, 0B10010110, 0B01010110, 
+		0B10101001, 0B01101001, 0B10011001, 0B01011001, 
+		0B10100101, 0B01100101, 0B10010101, 0B01010101,
+	};
 
-  // make a temporary separate buffer for the packet hexdump
-  // this part can be taken away as the decode procedure
-  // modifies the original data
-  uint8_t packetDump[65 * 3]; // LEN + FIFO is 64 bytes
-  for (int sIdx = 0; sIdx < data[0] + 1; sIdx++) {
-    packetDump[sIdx * 3 + 0] = XDIGIT[(data[sIdx] >> 4) & 0x0f];
-    packetDump[sIdx * 3 + 1] = XDIGIT[data[sIdx] & 0x0f];
-    packetDump[sIdx * 3 + 2] = ' ';
-  }
-  packetDump[2] = ':'; // just cosmetics
-  packetDump[data[0] * 3 + 2] = '\0';
+	// make a temporary separate buffer for the packet hexdump
+	// this part can be taken away as the decode procedure
+	// modifies the original data
+	uint8_t packetDump[65 * 3]; // LEN + FIFO is 64 bytes
+	for (int sIdx = 0; sIdx < data[0] + 1; sIdx++) {
+		packetDump[sIdx * 3 + 0] = XDIGIT[(data[sIdx] >> 4) & 0x0f];
+		packetDump[sIdx * 3 + 1] = XDIGIT[data[sIdx] & 0x0f];
+		packetDump[sIdx * 3 + 2] = ' ';
+	}
+	packetDump[2] = ':'; // just cosmetics
+	packetDump[data[0] * 3 + 2] = '\0';
 
-  tmstamp = 0;
+	tmstamp = 0;
 	// data[0] contains packet length + 2 (LQI and RSSI) thus we're trying
 	// to decode data from data[1] minus last TWO bytes
-  for (int sIdx = 1; sIdx < data[0] - 1; sIdx++) {
-    int nibble = 0x10; // 0x10 is invalid as nibble is 0..f
-    for (int idx = 0; idx < 0x10; idx++) {
-      if (XLAT[idx] == data[sIdx]) {
-        nibble = idx; // decode a nibble 8bit -> 4bit
-        break;
-      }
-    }
-    if ((nibble & 0xf0) != 0) {
-      syslog(LOG_MAKEPRI(LOG_DAEMON, LOG_WARNING),
-          "Package can't be decoded (bad nibble): %s", packetDump);
-      return tmstamp; // nibble can't be decoded; packet is invalid
-    }
-    data[sIdx] = nibble;
-  }
-  if (chkSum() != getChkSum()) {
-    syslog(LOG_MAKEPRI(LOG_DAEMON, LOG_WARNING),
-        "Package can't be decoded (bad chksum): %s", packetDump);
-    return tmstamp; // chkSum doesn't match; packet is invalid
-  }
+	for (int sIdx = 1; sIdx < data[0] - 1; sIdx++) {
+		int nibble = 0x10; // 0x10 is invalid as nibble is 0..f
+		for (int idx = 0; idx < 0x10; idx++) {
+			if (XLAT[idx] == data[sIdx]) {
+				nibble = idx; // decode a nibble 8bit -> 4bit
+				break;
+			}
+		}
+		if ((nibble & 0xf0) != 0) {
+			syslog(LOG_MAKEPRI(LOG_DAEMON, LOG_WARNING),
+					"Package can't be decoded (bad nibble): %s", packetDump);
+			return tmstamp; // nibble can't be decoded; packet is invalid
+		}
+		data[sIdx] = nibble;
+	}
+	if (chkSum() != getChkSum()) {
+		syslog(LOG_MAKEPRI(LOG_DAEMON, LOG_WARNING),
+				"Package can't be decoded (bad chksum): %s", packetDump);
+		return tmstamp; // chkSum doesn't match; packet is invalid
+	}
 
-  for (int sIdx = 0; sIdx < data[0] + 1; sIdx++) {
-    packetDump[sIdx * 3 + 0] = XDIGIT[(data[sIdx] >> 4) & 0x0f];
-    packetDump[sIdx * 3 + 1] = XDIGIT[data[sIdx] & 0x0f];
-    packetDump[sIdx * 3 + 2] = ' ';
-  }
-  packetDump[2] = ':'; // just cosmetics
-  packetDump[data[0] * 3 + 2] = '\0';
+#ifdef CC1101_DEBUG_LOGGING
+	for (int sIdx = 0; sIdx < data[0] + 1; sIdx++) {
+		packetDump[sIdx * 3 + 0] = XDIGIT[(data[sIdx] >> 4) & 0x0f];
+		packetDump[sIdx * 3 + 1] = XDIGIT[data[sIdx] & 0x0f];
+		packetDump[sIdx * 3 + 2] = ' ';
+	}
+	packetDump[2] = ':'; // just cosmetics
+	packetDump[data[0] * 3 + 2] = '\0';
 
 	syslog(LOG_MAKEPRI(LOG_DAEMON, LOG_DEBUG),
 			"packet: %s", packetDump);
+#endif
 
-  return (tmstamp = time(NULL));
+	return (tmstamp = time(NULL));
 }
 uint32_t CC1101DTA::getRCID() {
   return (getSensorId() << 16) | (getChannel() << 8) | getRollingCode();
